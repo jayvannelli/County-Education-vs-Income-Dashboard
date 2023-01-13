c03e69f2a5ae4718fe4280f17baf820f62add385
jayvannelli <jaymvannelli@gmail.com>
2023-01-13 10:06:30 -0500

    Clean up file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,17 +7,6 @@ def main():
 
     df = get_data()
     st.dataframe(df)
-    state_selection = st.selectbox(label="Select state:",
-                                   options=df['state'].unique())
-
-    state_df = df.loc[df['state'] == state_selection]
-    st.dataframe(state_df)
-
-    st.bar_chart(state_df, x="county", y="per_capita_personal_income_2019")
-
-    multiple_state_selection = st.multiselect(label="Select states:",
-                                              options=df['state'].unique(),
-                                              default=df['state'].unique()[:5])
 
 
 if __name__ == "__main__":
