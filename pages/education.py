abdf020a9b485cbef2953f765f4060384781c7e1
jayvannelli <jaymvannelli@gmail.com>
2023-01-15 07:53:04 -0500

    Add progress.

--- pages/education.py
+++ pages/education.py
@@ -1,12 +1,36 @@
+import plost
 import streamlit as st
 from src.data import get_data
 
 
 def main():
-    st.title("Education Data")
+    st.title("Education Data by State")
 
     df = get_data()
-    st.dataframe(df)
+
+    left_column, right_column = st.columns(2)
+    with left_column:
+        state_selection = st.selectbox("Select state:", options=df['state'].unique())
+        state_df = df.loc[df['state'] == state_selection]
+
+    with right_column:
+        display_type = st.selectbox("Display value as:", options=["Percentage", "Numbers"])
+
+    st.write("---")
+
+    st.subheader(f"{state_selection} Associate Degree Data")
+
+    if display_type == "Percentage":
+        st.bar_chart(state_df, x="county", y="associate_degree_percentage_2016_2020")
+    else:
+        st.bar_chart(state_df, x="county", y="associate_degree_numbers_2016_2020")
+
+    st.subheader(f"{state_selection} Bachelor Degree Data")
+
+    if display_type == "Percentage":
+        st.bar_chart(state_df, x="county", y="bachelor_degree_percentage_2015_2019")
+    else:
+        st.bar_chart(state_df, x="county", y="bachelor_degree_numbers_2016_2020")
 
     associate_degree_numbers = df['associate_degree_numbers_2016_2020']
     bachelor_degrees_numbers = df['bachelor_degree_numbers_2016_2020']
