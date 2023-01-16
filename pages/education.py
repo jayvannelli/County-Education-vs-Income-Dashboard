4fcee86a100dbd53e001cb0e4a3db82959571975
jayvannelli <jaymvannelli@gmail.com>
2023-01-15 20:32:59 -0500

    Improve education page subheaders.

--- pages/education.py
+++ pages/education.py
@@ -18,14 +18,18 @@ def main():
 
     st.write("---")
 
-    st.subheader(f"{state_selection} Associate Degree Data")
+    st.subheader(
+        f"{state_selection} Associate Degree Per Capita ({'%' if display_type == 'Percentage' else 'Quantity'})"
+    )
 
     if display_type == "Percentage":
         st.bar_chart(state_df, x="county", y="associate_degree_percentage_2016_2020")
     else:
         st.bar_chart(state_df, x="county", y="associate_degree_numbers_2016_2020")
 
-    st.subheader(f"{state_selection} Bachelor Degree Data")
+    st.subheader(
+        f"{state_selection} Bachelor Degree Per Capita ({'%' if display_type == 'Percentage' else 'Quantity'})"
+    )
 
     if display_type == "Percentage":
         st.bar_chart(state_df, x="county", y="bachelor_degree_percentage_2015_2019")
