8242087d27b49ed7273bc63d3126ce417fe3bdb4
jayvannelli <jaymvannelli@gmail.com>
2023-01-16 11:09:56 -0500

    Clean up code.

--- pages/02_📚_Education.py
+++ pages/02_📚_Education.py
@@ -15,21 +15,18 @@ def main():
 
     with right_column:
         display_type = st.selectbox("Display value as:", options=["Percentage", "Numbers"])
+        _subheader_display_type = '%' if display_type == 'Percentage' else 'Quantity'
 
     st.write("---")
 
-    st.subheader(
-        f"{state_selection} Associate Degree by County ({'%' if display_type == 'Percentage' else 'Quantity'})"
-    )
+    st.subheader(f"Associate Degree by County ({_subheader_display_type})")
 
     if display_type == "Percentage":
         st.bar_chart(state_df, x="county", y="associate_degree_percentage_2016_2020")
     else:
         st.bar_chart(state_df, x="county", y="associate_degree_numbers_2016_2020")
 
-    st.subheader(
-        f"{state_selection} Bachelor Degree by County ({'%' if display_type == 'Percentage' else 'Quantity'})"
-    )
+    st.subheader(f"Bachelor Degree by County ({_subheader_display_type})")
 
     if display_type == "Percentage":
         st.bar_chart(state_df, x="county", y="bachelor_degree_percentage_2015_2019")
