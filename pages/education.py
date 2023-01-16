ac59369df7ca69878897b82fc367b112bd61362d
jayvannelli <jaymvannelli@gmail.com>
2023-01-15 20:33:55 -0500

    Fix mistake.

--- pages/education.py
+++ pages/education.py
@@ -19,7 +19,7 @@ def main():
     st.write("---")
 
     st.subheader(
-        f"{state_selection} Associate Degree Per Capita ({'%' if display_type == 'Percentage' else 'Quantity'})"
+        f"{state_selection} Associate Degree by County ({'%' if display_type == 'Percentage' else 'Quantity'})"
     )
 
     if display_type == "Percentage":
@@ -28,7 +28,7 @@ def main():
         st.bar_chart(state_df, x="county", y="associate_degree_numbers_2016_2020")
 
     st.subheader(
-        f"{state_selection} Bachelor Degree Per Capita ({'%' if display_type == 'Percentage' else 'Quantity'})"
+        f"{state_selection} Bachelor Degree by County ({'%' if display_type == 'Percentage' else 'Quantity'})"
     )
 
     if display_type == "Percentage":
