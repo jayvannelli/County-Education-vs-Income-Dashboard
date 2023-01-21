0c3f46619628f23cc9399f0d63e629cd84df9120
jayvannelli <jaymvannelli@gmail.com>
2023-01-21 10:02:13 -0500

    Remove county selection for now.

--- pages/04_⛰️_State_Data.py
+++ pages/04_⛰️_State_Data.py
@@ -1,21 +1,20 @@
 import streamlit as st
+import pandas as pd
+
+from src.data import get_data
 from src.states import get_state_abbreviations, state_counties
 
 
 def main():
     st.title("State Specific Data")
 
+    df = get_data()
+
     all_state_abbvs = get_state_abbreviations()
 
     state_selection = st.selectbox("Select state", options=all_state_abbvs)
-    selected_state_counties = state_counties(state_selection)
-
-    county_selections = st.multiselect(label="Select county/counties",
-                                       options=selected_state_counties,
-                                       default=selected_state_counties[:5])
-
-    st.write(county_selections)
-    st.write(selected_state_counties)
+    state_df = df.loc[df['state'] == state_selection]
+    st.dataframe(state_df)
 
 
 if __name__ == "__main__":
